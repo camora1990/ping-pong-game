4ce54db282971ed9097906ac02ed9f1f5eceb54b
camora1990 <moorales.camilo@gmail.com>
2022-07-13 15:35:03 -0500

    [add] clase board para construccion del tablero del ping pong

---
+++ js/components/board.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * Clase encargada de crear el tabledo del juego 
+ * @class
+ */
+
+export class Board {
+
+    /**
+     * @constructor
+     * @param {number} width - ancho del tabledo canvan
+     * @param {*number} height - alto del tablero canvan
+     */
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.playing = false;
+    this.game_over = false;
+    this.bars = [];
+    this.ball = null;
+    this.playing = false;
+  }
+
+  /**
+   * @returns - retorna los elementos bars y ball del board
+   */
+  get elementsBoard() {
+    const elements = this.bars.map((e) => e);
+    elements.push(this.ball);
+    return elements;
+  }
+}
